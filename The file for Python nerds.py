0522d0ccab0674c322bafd94e64b20c3ce1de5b0
GFX93 <106411916+GFX93@users.noreply.github.com>
2022-05-27 20:58:27 -0400

    Create The file for Python nerds.py
    The file for nerds like me to look at

---
+++ The file for Python nerds.py
@@ -0,0 +1,19 @@
+from os import system as sys
+from random import randint
+from time import sleep as wait
+def clear: sys('clear'):
+print('.                   -------*')
+print('Welcome to DOOM QUEST SWORD')
+print('.                     *---------')
+wait(3)
+print('Ugh, Ill finish this in the morning')
+wait(0.5)
+print('Its 8:57')
+wait(0.5)
+print('I'm Tired.)
+wait(0.5)
+print('If you see this, Respect')
+wait(2)
+clear()
+wait(1)
+quit()
